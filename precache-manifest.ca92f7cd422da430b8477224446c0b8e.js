1b9f410916ba038d8826cf4c8c0ec98e55d4f547
Vipansh Thakur <vipansh@github.com>
2020-10-11 15:54:29 +0530

    Updates

---
+++ precache-manifest.ca92f7cd422da430b8477224446c0b8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c74ccd4b9e241cf02a0adeb5c6d2555b",
+    "url": "/pokemone-card/index.html"
+  },
+  {
+    "revision": "28f7412a29230fcbd24d",
+    "url": "/pokemone-card/static/css/main.bdc6deb3.chunk.css"
+  },
+  {
+    "revision": "ee88d1b82f3e31fcda9a",
+    "url": "/pokemone-card/static/js/2.89288cbb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokemone-card/static/js/2.89288cbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28f7412a29230fcbd24d",
+    "url": "/pokemone-card/static/js/main.08e08fac.chunk.js"
+  },
+  {
+    "revision": "c45c13bd00c5adea7e0e",
+    "url": "/pokemone-card/static/js/runtime-main.7a1871c6.js"
+  }
+]);
